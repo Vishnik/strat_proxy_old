e001e063f9c17980a9dea2cd624abbc9b2437c61
Taras Vishnik <>
2024-02-08 23:39:28 +0700

    рефакторинг server.js

--- server.js
+++ server.js
@@ -1,41 +1,36 @@
-var net = require('net');
+const net = require('net');
 
 // My libraries
-var sm = require('./lib/stratumitm.js');
-var delegator = require('./lib/delegator');
-var logger = require('./lib/logger');
-var confreader = require('./lib/config');
+const sm = require('./lib/stratumitm.js');
+const delegatorRequire = require('./lib/delegator');
+const logger = require('./lib/logger');
 
-// Create the log
-var config = (new confreader.ConfigReader(confreader.FALLBACK_CONFIG)).read(
-	'config.json', 'config.sample.json');
+const config = require('./config.sample.json');
 
-var log = new logger.Logger([], config.logger ? config.logger.params : null);
-var rewriterClass = require(config.rewriter);
+
+const log = new logger.Logger([], config.logger ? config.logger.params : null);
+const rewriterClass = require('./lib/rewrite/rewrite.credentials');
 
 // Load the rewriter
-var delegator = new delegator.Delegator();
-var rewriter = new rewriterClass.Rewriter(config.params, delegator, log);
+const delegator = new delegatorRequire.Delegator();
+const rewriter = new rewriterClass.Rewriter(config.params, delegator, log);
 
 // Set up servers on each port
-var stratumitm = new sm.StratuMITM(delegator, rewriter, log);
+const stratumitm = new sm.StratuMITM(delegator, rewriter, log);
 
-var servers = [];
-for (var i = 0; i < config.ports.length; i++) {
-	var name = 'Port=' + config.ports[i];
-	servers.push((function (port) {
-		var server = {
-			'socket': net.createServer(),
-			'name': name,
-			'log': log.part(name)
-		};
-		server.socket.on('connection', stratumitm.getHandler(name));
-		server.socket.on('error', function (e) {
-			server.log.error(e);
-		});
-		server.socket.on('listening', function () {
-			server.log.log('Listening on ' + server.socket.address().port);
-		});
-		server.socket.listen(port);
-	})(config.ports[i]));
-}
+config.ports.forEach((port) => {
+	const name = 'Port=' + port;
+	const socket = net.createServer();
+	const logger = log.part(name);
+
+	socket.on('connection', stratumitm.getHandler(name));
+	socket.on('error', function (e) {
+		log.error(e);
+	});
+
+	socket.on('listening', function () {
+		logger.log('Listening on ' + socket.address().port);
+	});
+
+	socket.listen(port);
+});
